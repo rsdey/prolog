e2e6592f2beb3bd42e5c52a1f9db3b264d6a5180
Ranadeep Dey <ranadeepdey01@gmail.com>
2020-08-07 21:53:03 +0530

    r

---
+++ 4B/drug.pl
@@ -0,0 +1,18 @@
+%considering a medicine preparation:
+
+%the following the constituent drugs for making medicines:
+
+drug(clobazam).
+drug(apidra).
+drug(intuniv).
+drug(zarocolyn).
+
+%the following are the targetted areas:
+
+target(pdli).
+target(her2).
+target(brag).
+target(hmie5).
+
+medicineA(clobazam,her2).
+medicineB(zarocolyn,her2,pdli2).
